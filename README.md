bba7a4ba1a490527292e69d24a05f67a40fdc2e7
TrilaryQuispe <111787242+Trilarydev@users.noreply.github.com>
2022-12-15 23:49:54 -0500

    login

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# H221S2_001
+# -H221S2_16.
+Hackathon 2022-II / AS221_S2 (Caso: Proyecto Landing Page)
---
+++ js/Contactanos.js
@@ -0,0 +1,59 @@
+const url = "http://54.81.15.104:3006/api/contactanos"; //Cambiar por ip del servidor en caso la db este en otra maquina. (54.81.15.104/)
+const formContactanos = document.getElementById("formulario-contactanos");
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const asunto = document.getElementById("asunto");
+const descripcion = document.getElementById("descripcion");
+formContactanos.addEventListener('submit',
+(e) => {
+e.preventDefault();
+if (nombre.value == "" || correo.value == "" || asunto.value == "" || descripcion.value == "") {
+    mostrarMensajeError();
+    return false;
+    } else {
+    console.log("Todos los campos están completos");
+    let configuracion = {
+    method: 'POST',
+    headers: {
+    'content-Type': 'application/json'
+    },
+    body: JSON.stringify(
+    {
+    nombre: nombre.value,
+    correo: correo.value,
+    asunto: asunto.value,
+    descripcion: descripcion.value
+    }
+    )
+    };
+    fetch(url, configuracion)
+    .then(response => {
+    // console.log('Respuesta del servidor: ', response);
+    response.json();
+    mostrarMensajeCorrecto();
+    limpiarCampos();
+    });
+    }
+    }
+    );
+    /*Mensaje de confirmacion de matricula*/
+    function mostrarMensajeCorrecto() {
+        let mensaje = document.getElementById("snackbar");
+        mensaje.className = "show-correcto";
+        mensaje.innerText = "Asunto resgistrado con exito ";
+        setTimeout(function () { mensaje.className = mensaje.className.replace("show-correcto",
+        ""); }, 3000);
+        }
+        function mostrarMensajeError() {
+        let mensaje = document.getElementById("snackbar");
+        mensaje.className = "show-error";
+        mensaje.innerText = "Valores incorrectos "
+        setTimeout(function () { mensaje.className = mensaje.className.replace("show-error", "");
+        }, 3000);
+        }
+        function limpiarCampos(){
+        nombre.value = "";
+        correo.value = "";
+        asunto.value = "";
+        descripcion.value = "";
+        }
